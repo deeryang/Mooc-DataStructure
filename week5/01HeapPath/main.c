fc67953071ef4b4fc21215c8140ee2fa9b8ff0c7
deeryang <745898859@qq.com>
2018-11-20 21:22:03 +0800

    add

---
+++ week5/01HeapPath/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXN 1001;
+#define MINH -10001;
+
+int H[1001];
+int size;
+
+void Create() {
+    size = 0;
+    H[0] = MINH;
+
+}
+
+void Insert(int X) {
+
+    int i;
+    for ( i = ++size; H[i/2] > X; i /= 2) {
+        H[i] = H[i/2];
+    }
+    H[i] = X;
+}
+
+int main() {
+    int N, M, x;
+    scanf("%d %d", &N, &M);
+    Create();
+    for (int i = 0; i < N; i ++) {
+        scanf("%d", &x);
+        Insert(x);
+    }
+
+    int j;
+    for (int i = 0; i < M; i ++) {
+        scanf("%d", &j);
+        printf("%d", H[j]);
+        while (j > 1) {
+            j /= 2;
+            printf(" %d", H[j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//MinHeap CreateHeap(int MaxSize) {
+//    MinHeap H = (MinHeap) malloc(sizeof(struct HNode));
+//    H->Data = (ElementType*) malloc(sizeof(struct ElementType) * (MaxSize + 1));
+//    H->size = 0;
+//    H->Capacity = MaxSize;
+//    H->Data[0] = MAXDATA;
+
+//    return H;
+//}
+
